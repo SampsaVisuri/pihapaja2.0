ca8b692867d5c20c4b5e4d1acf5155e264b9651b
SampsaVisuri <59508119+SampsaVisuri@users.noreply.github.com>
2021-03-05 07:15:31 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,144 @@
+document.getElementById("show").addEventListener("click", function () {
+
+    document.getElementById("refe4").style.display = 'flex';
+    document.getElementById("refe5").style.display = 'flex';
+    document.getElementById("refe6").style.display = 'flex';
+    document.getElementById("refe7").style.display = 'flex';
+    document.getElementById("refe8").style.display = 'flex';
+    document.getElementById("refe9").style.display = 'flex';
+
+    document.getElementById("show").style.display = 'none';
+    document.getElementById("hide").style.display = 'block';
+})
+
+document.getElementById("hide").addEventListener("click", function () {
+
+    document.getElementById("refe4").style.display = 'none';
+    document.getElementById("refe5").style.display = 'none';
+    document.getElementById("refe6").style.display = 'none';
+    document.getElementById("refe7").style.display = 'none';
+    document.getElementById("refe8").style.display = 'none';
+    document.getElementById("refe9").style.display = 'none';
+
+    document.getElementById("show").style.display = 'block';
+    document.getElementById("hide").style.display = 'none';
+})
+
+
+
+
+document.getElementById("refe1").addEventListener("mouseover", function () {
+
+
+    document.getElementById("refe1-text").style.display = 'block';
+
+})
+
+document.getElementById("refe1").addEventListener("mouseout", function () {
+
+    if (window.innerWidth > 899) {
+        document.getElementById("refe1-text").style.display = 'none';
+    }
+
+})
+
+
+// REFE 2
+
+document.getElementById("refe2").addEventListener("mouseover", function () {
+
+    document.getElementById("refe2-text").style.display = 'block';
+})
+document.getElementById("refe2").addEventListener("mouseout", function () {
+
+    if (window.innerWidth > 899) {
+        document.getElementById("refe2-text").style.display = 'none';
+    }
+})
+
+// REFE 3
+document.getElementById("refe3").addEventListener("mouseover", function () {
+
+    document.getElementById("refe3-text").style.display = 'block';
+})
+document.getElementById("refe3").addEventListener("mouseout", function () {
+
+    if (window.innerWidth > 899) {
+        document.getElementById("refe3-text").style.display = 'none';
+    }
+})
+
+// REFE 4
+document.getElementById("refe4").addEventListener("mouseover", function () {
+
+    document.getElementById("refe4-text").style.display = 'block';
+})
+document.getElementById("refe4").addEventListener("mouseout", function () {
+
+    if (window.innerWidth > 899) {
+        document.getElementById("refe4-text").style.display = 'none';
+    }
+})
+
+// REFE 5
+document.getElementById("refe5").addEventListener("mouseover", function () {
+
+    document.getElementById("refe5-text").style.display = 'block';
+})
+document.getElementById("refe5").addEventListener("mouseout", function () {
+
+    if (window.innerWidth > 899) {
+        document.getElementById("refe5-text").style.display = 'none';
+    }
+})
+
+// REFE 6
+document.getElementById("refe6").addEventListener("mouseover", function () {
+
+    document.getElementById("refe6-text").style.display = 'block';
+})
+document.getElementById("refe6").addEventListener("mouseout", function () {
+
+    if (window.innerWidth > 899) {
+        document.getElementById("refe6-text").style.display = 'none';
+    }
+})
+
+
+// REFE 7
+document.getElementById("refe7").addEventListener("mouseover", function () {
+
+    document.getElementById("refe7-text").style.display = 'block';
+})
+document.getElementById("refe7").addEventListener("mouseout", function () {
+
+    if (window.innerWidth > 899) {
+        document.getElementById("refe7-text").style.display = 'none';
+    }
+})
+
+
+// REFE 8
+document.getElementById("refe8").addEventListener("mouseover", function () {
+
+    document.getElementById("refe8-text").style.display = 'block';
+})
+document.getElementById("refe8").addEventListener("mouseout", function () {
+
+    if (window.innerWidth > 899) {
+        document.getElementById("refe8-text").style.display = 'none';
+    }
+})
+
+// REFE 9
+document.getElementById("refe9").addEventListener("mouseover", function () {
+
+    document.getElementById("refe9-text").style.display = 'block';
+})
+document.getElementById("refe9").addEventListener("mouseout", function () {
+
+    if (window.innerWidth > 899) {
+        document.getElementById("refe9-text").style.display = 'none';
+    }
+})
+
